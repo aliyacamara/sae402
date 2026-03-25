1206024932ed9b8ca09c52e78f3c130d9034ed9b
aliyacamara <aliyacamara60@gmail.com>
2026-03-25 11:09:52 +0100

    Add PlayerDebugPosition script for editor debugging functionality

---
+++ Assets/Scripts/PlayerDebugPosition.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+ 
+public class PlayerDebugPosition : MonoBehaviour
+{
+    #if UNITY_EDITOR
+    [SerializeField]
+    private Transform debugTransform;
+ 
+ 
+    void Awake()
+    {
+        if (debugTransform)
+        {
+            transform.position = debugTransform.position;
+        }
+    }
+    #endif
+}
+ 
+ 
